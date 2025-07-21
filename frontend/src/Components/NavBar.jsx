cee54dfee4890ddbc37c4118b927b1fc35a01239
TimothyRF <timothy.furlong@my.uwrf.edu>
2025-07-21 16:37:04 -0500

    added a navbar component and files for jsx components/pages

---
+++ frontend/src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import{link} from "react-router-dom"
+function NavBar(){
+    return <nav className="navbar">
+        <div className="navbar-logo">
+            Comp Sci Social club
+        </div>
+
+
+    </nav>
+}
+
+export default NavBar
