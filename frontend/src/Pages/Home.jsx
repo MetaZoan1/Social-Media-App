86fc684f7c55e3efb43e498958038e2c22c8fc94
TimothyRF <timothy.furlong@my.uwrf.edu>
2025-07-31 15:28:44 -0500

    beginning to as search for user functionality to home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,26 @@
 import NavBar from "../Components/NavBar"
 import Post from "../Components/Post"
+import {useState} from "react"
 
 function Home(){
+    const[userSearch, setUserSearch] = useState("");
+    const searchAction = (e) => {
+        e.preventDefault()
+    }
+    
+    
+    return(
+    
     <div className= "Home">
-        
+        <form onSubmit={handleSearch} className= "search for user">
+            <input type="text" placeholder= "search for users" className="search-input" value= {userSearch}
+                onChange={(e) => setSearchQuery(e.target.value)}/>
+            <button type="submit" className="searchButton">Search</button>
+        </form>
 
+    
+    
     </div>
+
+    )
 }
